4ac4101771e844d48e7f68927f89ef44992af608
marcos <marcoslemeborbafilho@gmail.com>
2020-08-19 22:35:52 -0300

    Fixing Indentatons on TestModels

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,6 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-
         email = 'test@londonappdev.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
@@ -14,6 +13,5 @@ class ModelTests(TestCase):
             password=password
         )
 
-    #self.assertEqual(user.email, email)
-    self.assertEqual(user.email, email)
-    self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
