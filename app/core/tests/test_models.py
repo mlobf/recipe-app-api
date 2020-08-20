fef3f2692ec76c318b792d1e03e1e74b4b7c7c21
marcos <marcoslemeborbafilho@gmail.com>
2020-08-19 22:51:31 -0300

    commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,12 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def new_user_email_normalized(self):
+        """
+        Test the email user for a new user is normalized
+        """
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test1234')
+
+        self.assertEqual(user.email, email.lower())
